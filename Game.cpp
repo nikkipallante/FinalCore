6e34429c614cc71e54507ac49fda15f49391a535
bskyla <113308483+bskyla@users.noreply.github.com>
2022-11-21 21:41:11 -0500

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -65,35 +65,35 @@ bool Game::isValidPickupList(const string& pickupList, const int pickupFloorNum)
      }
     //checks to see if any person id is negative
     for (int k = 0; k < pickupList.length(); k++)
-    {
-        if (pickupList[k] < 0){
-            valid = false;
-        }
-    }
-    //Checks to see if pick up list is less than elevator capacity
-    if (pickupList.length() > ELEVATOR_CAPACITY){
-        valid = false;
-    }
-    // checks to see if pickupList is less than number of people on the floor
-    for (int m = 0; m < pickupList.length(); m++)
-    {
-        if (pickupList[m] >= building.getFloorByFloorNum(pickupFloorNum).getNumPeople()){
-            valid = false;
-        }
-    }
-    //checks to see if the elevator is being called in two directions, if so, request not valid DEFINITELY WRONG
-    int count = 0;
-    for (int s = 0; s < pickupList.length(); s++)
-    {
-        if (building.getFloorByFloorNum(pickupFloorNum).getPersonByIndex(s).getTargetValue() - pickupFloorNum > 0)
         {
-            count++;
+            if (pickupList[k] < 0){
+                valid = false;
+            }
         }
-    }
-    if (count != 0 && count != pickupList.length())
-    {
-        valid = false;
-    }
+        //Checks to see if pick up list is less than elevator capacity
+        if (pickupList.length() > ELEVATOR_CAPACITY){
+            valid = false;
+        }
+        // checks to see if pickupList is less than number of people on the floor
+        for (int m = 0; m < pickupList.length(); m++)
+        {
+            if (pickupList[m] >= building.getFloorByFloorNum(pickupFloorNum).getNumPeople()){
+                valid = false;
+            }
+        }
+        //checks to see if the elevator is being called in two directions, if so, request not valid DEFINITELY WRONG
+        int count = 0;
+        for (int s = 0; s < pickupList.length(); s++)
+        {
+            if (building.getFloorByFloorNum(pickupFloorNum).getPersonByIndex(s).getTargetFloor() - pickupFloorNum > 0)
+            {
+                count++;
+            }
+        }
+        if (count != 0 && count != pickupList.length())
+        {
+            valid = false;
+        }
      return valid;
 }
 
