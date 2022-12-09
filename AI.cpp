07059f29e1ab694f18ce486b1bc0e01c07f54489
nikkipallante <113399718+nikkipallante@users.noreply.github.com>
2022-12-08 23:34:52 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -16,11 +16,31 @@
 // This file is used only in the Reach, not the Core.
 // You do not need to make any changes to this file for the Core
 
-string getAIMoveString(const BuildingState& buildingState) {
-    return "";
+string getAIMoveString(const BuildingState& buildingState)
+{
+    //find floor with largest number of people
+    int floorMostPeople = 0;
+    for(int i = 0; i < NUM_FLOORS; i++)
+    {
+        if(buildingState.floors[i].numPeople > buildingState.floors[floorMostPeople].numPeople)
+        {
+            floorMostPeople = i;
+        }
+    }
+    int elevatorToUse = 0;
+    for(int i = 0; i < NUM_ELEVATORS; i++)
+    {
+        if(!buildingState.elevators[i].isServicing)
+        {
+            elevatorToUse = i;
+            break;
+        }
+    }
+    return "e" + to_string(elevatorToUse) + "f" + to_string(floorMostPeople);
 }
 
 string getAIPickupList(const Move& move, const BuildingState& buildingState, 
-                       const Floor& floorToPickup) {
+                       const Floor& floorToPickup)
+{
     return "";
 }
